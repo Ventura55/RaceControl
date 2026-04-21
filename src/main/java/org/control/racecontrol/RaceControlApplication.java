f8069701c7bbc16cba63b677da31ad43b8f47ffd
Ventura55 <diegoventurduq@gmail.com>
2026-04-21 12:49:05 +0200

    Initial commit

---
+++ src/main/java/org/control/racecontrol/RaceControlApplication.java
@@ -0,0 +1,13 @@
+package org.control.racecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/control/racecontrol/RaceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.control.racecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
